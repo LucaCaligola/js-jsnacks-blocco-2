d026ea984d496281009a2313554739a3c85f1535
LucaCaligola <luca.caligola@gmail.com>
2023-11-17 12:06:18 +0100

    working on the snack

--- snack-2/script.js
+++ snack-2/script.js
@@ -1,3 +1,15 @@
 // Generatore di "nomi cognomi" casuali: il Grande Gatsby ha una lista di nomi e una 
 // lista di cognomi, 
 // e da queste vuole generare una falsa lista di invitati con nome e cognome.
+
+// Creare due array (nomi, cognomi) con i nomi e cognomi che conosciamo
+// Creare una lista (array) dove ci saranno i nomi e cognome sommati
+// Implementare un While che finchè ci sono nomi e cognomi all'interno della lista li somma 
+// Aggiungere alla lista (array) la somma tra nomi e cognomi
+
+const nomi = ['gino', 'gina','mario','maria'];
+const cognomi = ['rossini','giallini','bluetti','verdini'];
+
+let array = [];
+
+while ()
