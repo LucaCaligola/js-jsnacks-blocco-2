a6c1d26e3b4e3296acd5fdb8d4b0c17bba7c5f61
LucaCaligola <luca.caligola@gmail.com>
2023-11-21 12:14:03 +0100

    working on the function

--- snack-6/script.js
+++ snack-6/script.js
@@ -1,3 +1,13 @@
 // Chiedo all'utente un numero e genero attraverso una funzione un numero 
 // di quadrati equivalente 
 // al numero inserito dall'utente nel DOM, in qualsiasi forma. 
+
+
+const userNumber = parseInt(prompt("scegli un numero"));
+console.log (userNumber);
+
+function getSquare () {
+    const newSquareElement = document.createElement('div')
+    newSquareElement.classList.add('square');
+    return newSquareElement;
+}
