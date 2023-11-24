aba894f106b029b448f1921ddf11912fcda01c6d
LucaCaligola <luca.caligola@gmail.com>
2023-11-24 12:31:31 +0100

    working on the objects

--- snack-7/script.js
+++ snack-7/script.js
@@ -1,2 +1,41 @@
 // Crea 10 oggetti che rappresentano una zucchina, indicandone 
 // per ognuno varietà, peso e lunghezza. Calcola quanto pesano tutte le zucchine
+
+
+const zucchinaRomana = {
+    nome: 'Zucchina Romana',
+    peso: 400,
+    lunghezza: 8,
+}
+
+const zucchinaFiorentina = {
+    nome:'Zucchina Fiorentina',
+    peso: 540,
+    lunghezza: 12,
+}
+
+const zucchinaTonda = {
+    nome: 'Zucchina Tonda',
+    peso: 350,
+    lunghezza: 6,
+}
+
+const zucchinaNapoletana = {
+    nome: 'Zucchina Napoletana',
+    peso: 450,
+    lunghezza: 10, 
+}
+
+const zucchinaGialla = {
+    nome: 'Zucchina Gialla',
+    peso: 500,
+    lunghezza: 12,
+    
+}
+
+const zucchinaGNera = {
+    nome: 'Zucchina Nera',
+    peso: 300,
+    lunghezza: 11,
+    
+}
